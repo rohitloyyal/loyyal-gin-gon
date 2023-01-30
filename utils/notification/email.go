461773d5c1f6f4aee28a4172c5ec6f07065f495c
Rohit Roy <115474172+rohitloyyal@users.noreply.github.com>
2023-01-30 17:56:21 +0400

    Update email.go

--- utils/notification/email.go
+++ utils/notification/email.go
@@ -5,33 +5,15 @@ import (
 )
 
 func SendEmailNotification() error {
-	// auth := smtp.PlainAuth("", "apikey", "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", "smtp.sendgrid.net")
-
-	// from := "no-reply@loyyal.net"
-	// to := []string{"rohit@loyyal.com"}
-
-	// msg := []byte("To: rohit@loyyal.com\r\n" +
-	// 	"Subject: Why aren’t you using Mailtrap yet?\r\n" +
-	// 	"\r\n" +
-	// 	"Here’s the space for our great sales pitch\r\n")
-
-	// err := smtp.SendMail("smtp.sendgrid.net:587", auth, from, to, msg)
-
-	// if err != nil {
-	// 	return err
-	// }
-
-	// return nil
-
 	m := gomail.NewMessage()
-	m.SetHeader("From", "no-reply@loyyal.net")
+	m.SetHeader("From", SMTP_FROM)
 	m.SetHeader("To", "rohit@loyyal.com", "rohit@loyyal.com")
 	m.SetAddressHeader("Cc", "rohitroyrr8@gmail.com", "Dan")
 	m.SetHeader("Subject", "Hello!")
 	m.SetBody("text/html", "Hello <b>Bob</b> and <i>Cora</i>!")
 	// m.Attach("/home/Alex/lolcat.jpg")
 
-	d := gomail.NewDialer("smtp.sendgrid.net", 587, "apikey", "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")
+	d := gomail.NewDialer(SMTP_HOST, SMTP_PORT, SMTP_USERNAME , SMTP_PASSWORD)
 
 	// Send the email to Bob, Cora and Dan.
 	if err := d.DialAndSend(m); err != nil {
